65baae9e358d9c262f2ed8468efc0439de688191
ellie.k <elliekoooo@gmail.com>
2023-07-06 00:39:13 -0700

    add solution

---
+++ java/src/climbStairs.java
@@ -0,0 +1,21 @@
+public class climbStairs {
+    class Solution {
+        public int climbStairs(int n) {
+            if(n <= 2)
+                return n;
+
+            int front = 1;
+            int back = 2;
+            int count = 0;
+
+            for(int i=2; i<n; i++){
+                count = front + back;
+                front = back;
+                back = count;
+            }
+
+
+            return count;
+        }
+    }
+}
